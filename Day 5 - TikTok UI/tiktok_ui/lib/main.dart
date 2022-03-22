5460d218401b60e47ce5d443bab21bfa55284350
Ini Jones <jonesinim@gmail.com>
2022-03-22 10:18:19 +0100

    Initial commit

---
+++ Day 5 - TikTok UI/tiktok_ui/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          canvasColor: Color(0xff282828)),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Discover",
+          ),
+          BottomNavigationBarItem(
+            icon: _CreateButton(),
+            label: "",
+          ),
+        ],
+      ),
+    );
+  }
+}
